a3b02fea0c3f18a0286276257f1f7d1d7ad6c5b5
Marina Zinchenko <amanda79116@gmail.com>
2022-10-06 23:26:46 +0300

    Added second test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,9 +8,9 @@ public class StatisticsService
      * @param incomes - array of incomes
      * @return - index of first max value
      */
-    public long findMax(long[] incomes) {
+    public long findMax(long[] incomes) { //{1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long current_max_index = 0;
-        long current_max = incomes[0];
+        long current_max = incomes[0]; //1
         for (long income : incomes)
             if (current_max < income)
                 current_max = income;
